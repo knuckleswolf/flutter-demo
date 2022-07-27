258c97566189546c677f8237f8e0fa7716d3e2f4
knuckleswolf <knuckleswolf@gmail.com>
2022-07-28 01:08:40 +0700

    TASK-4.4: Text and RichText

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,42 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #F5F5F5
+  static const Color background = Color(0xFFF5F5F5);
+
+  /// Color: #4CAF50
+  static const Color green = Color(0xFF4CAF50);
+
+  /// Color: #8F7C7E92
+  static const Color inactive = Color(0x8F7C7E92);
+
+  /// Color: #252849
+  static const Color main = Color(0xFF252849);
+
+  /// Color: #EF4343
+  static const Color red = Color(0xFFEF4343);
+
+  /// Color: #3B3E5B
+  static const Color secondary = Color(0xFF3B3E5B);
+
+  /// Color: #7C7E92
+  static const Color secondary2 = Color(0xFF7C7E92);
+
+  /// Color: #ffffff
+  static const Color white = Color(0xFFFFFFFF);
+
+  /// Color: #FCDD3D
+  static const Color yellow = Color(0xFFFCDD3D);
+}
--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/gen/colors.gen.dart';
 
 class SightListScreen extends StatefulWidget {
   const SightListScreen({super.key});
@@ -9,24 +10,71 @@ class SightListScreen extends StatefulWidget {
 
 class TextSettings {
   static const TextStyle style = TextStyle(
-    fontWeight: FontWeight.bold,
+    height: 1.125,
     fontSize: 32,
+    fontWeight: FontWeight.bold,
+    color: ColorName.main,
   );
 
-  static const String title = 'Список интересных мест';
+  static const String title = 'Список \nинтересных мест';
+}
+
+class TextWithStyle extends StatelessWidget {
+  const TextWithStyle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      TextSettings.title,
+      style: TextSettings.style,
+      textAlign: TextAlign.start,
+    );
+  }
+}
+
+class RichTextWithStyle extends StatelessWidget {
+  const RichTextWithStyle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: const TextSpan(
+        style: TextSettings.style,
+        children: [
+          TextSpan(
+            text: 'С',
+            style: TextStyle(
+              color: ColorName.green,
+            ),
+          ),
+          TextSpan(
+            text: 'писок \n',
+          ),
+          TextSpan(
+            text: 'и',
+            style: TextStyle(
+              color: ColorName.yellow,
+            ),
+          ),
+          TextSpan(
+            text: 'нтересных мест',
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: EdgeInsets.symmetric(vertical: 40, horizontal: 16),
-        child: Text(
-          TextSettings.title,
-          style: TextSettings.style,
-        ),
+      appBar: AppBar(
+        toolbarHeight: 112.0,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const TextWithStyle(), // or const RichTextWithStyle()
       ),
     );
   }
